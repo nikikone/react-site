e3b544c2d142e4b62f47e1e73e93081b162688d0
nikikone <87607801+nikikone@users.noreply.github.com>
2021-07-19 06:37:48 +1000

    Update carts.js

--- src/actions/carts.js
+++ src/actions/carts.js
@@ -18,3 +18,51 @@ export function cartsFetchData(url){
             .then(carts =>dispatch(cartsFetchDataSuccess(carts)))
     }
 }
+
+export function cartsFetchDataDelete(url){
+    return (dispatch)=>{
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(response =>{
+                if(!response.ok){
+                    throw new Error(response.statusText)
+                }
+                return response;
+            })
+            .then(response => response.json())
+            .then(carts =>dispatch(cartsFetchDataSuccess(carts)))
+    }
+}
+
+export function cartsFetchDataPutQuantity(url){
+    return (dispatch)=>{
+        fetch(url, {
+            method: 'PUT'
+        })
+            .then(response =>{
+                if(!response.ok){
+                    throw new Error(response.statusText)
+                }
+                return response;
+            })
+            .then(response => response.json())
+            .then(carts =>dispatch(cartsFetchDataSuccess(carts)))
+    }
+}
+
+export function cartsFetchDataPostProduct(url){
+    return (dispatch)=>{
+        fetch(url, {
+            method: 'POST'
+        })
+            .then(response =>{
+                if(!response.ok){
+                    throw new Error(response.statusText)
+                }
+                return response;
+            })
+            .then(response => response.json())
+            .then(carts =>dispatch(cartsFetchDataSuccess(carts)))
+    }
+}
