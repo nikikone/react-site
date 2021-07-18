afc4d542e7dfff78fbf6f87cab3d12c73f1683a6
nikikone <87607801+nikikone@users.noreply.github.com>
2021-07-19 06:35:03 +1000

    Update Basket.js

--- src/Basket.js
+++ src/Basket.js
@@ -2,21 +2,13 @@ import React from 'react'
 import { AppBar, Container, IconButton, Toolbar, Typography, Box, Paper, Grid, CardMedia, CardContent, Card, CardActions } from '@material-ui/core';
 import { alpha, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import imger from './logo.svg';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
 import { cartsFetchData, cartsFetchDataSuccess } from './actions/carts';
+import { cartsFetchDataDelete } from './actions/carts';
+import { cartsFetchDataPutQuantity } from './actions/carts';
 import { connect } from 'react-redux';
 import { useState } from 'react';
 import { useEffect, useRef } from 'react'
-import {useImage} from 'react-image'
 
-import {
-    BrowserRouter as Router,
-    Link,
-} from "react-router-dom"
-import { products } from './reducers/products';
 
 const useStyles = makeStyles((theme) => ({
     cardGrid: {
@@ -52,11 +44,6 @@ const useStyles = makeStyles((theme) => ({
     },
 }))
 
-/*["Аниме стафф", "Бенто, Зонты, Очки, Часы"],
-["Дакимакуры", ""],
-["Прочая атрибутика", "Гобелены, Катаны, Комиксы, Кошельки, Наушники, Пазлы, Плед, Постельное бельё, Футляр для очков"],
-["Косплей", "Костюмы и атрибутика"]*/
-//const cards = [];
 
 function Basket(props) {
     const [cards, setCards] = useState([]);
@@ -71,11 +58,7 @@ function Basket(props) {
             return (<Button size="small" color="primary" onClick={(e) => this.onclick(e)}>Перейти к оформлению товара</Button>);
         }
     }
-    const [age, setAge] = React.useState('');
 
-    const handleChange = (event) => {
-        setAge(event.target.value);
-    };
     function getProd(crv, id) {
         const prd = [{ name: "" }];
         if (crv.length > 2) {
@@ -85,8 +68,11 @@ function Basket(props) {
         }
     }
 
-    const handleUpdateQty = () => {
-
+    const handleUpdateQty = (id, num) => {
+        if (num >= 1) {
+            const url = "http://localhost/api/carts/update/" + id + "?quantity=" + num;
+            props.putQuantityCartsFetchData(url);
+        }
     }
 
     useEffect(() => {
@@ -105,6 +91,11 @@ function Basket(props) {
         setProduct(props.products);
     }, [props.products]);
 
+    const handleDelete = (id) => {
+        const url = "http://localhost/api/carts/delete/" + id;
+        props.deleteCartsFetchData(url);
+    }
+
     return (
         <div>
             <Container className={classes.cardGrid} maxWidth="lg" >
@@ -117,7 +108,7 @@ function Basket(props) {
                             <Paper className={classes.paper} /*</Grid>*className={classes.cardGrid}*/>
                                 <Grid container spacing={2}>
                                     <Grid item>
-                                    {product.length > 1 ? (<div><img src = {product[card.prod_id - 1].image}/></div>) : (<div></div>)}
+                                        {product.length > 1 ? (<div><img src={product[card.prod_id - 1].image} /></div>) : (<div></div>)}
                                     </Grid>
                                     <Grid item xs={12} sm container>
                                         <Grid item xs container direction="column" spacing={2}>
@@ -125,38 +116,21 @@ function Basket(props) {
                                                 <Typography gutterBottom variant="subtitle1">
                                                     {product.length > 1 ? (<div>{product[card.prod_id - 1].name}</div>) : (<div></div>)}
                                                 </Typography>
-                                                <Button type='button' size='small' onClick={() => handleUpdateQty(card.quantity - 1)} > - </Button>
-                                                <Typography> {card.quantity} </Typography>
-                                                <Button type='button' size='small' onClick={() => handleUpdateQty(card.quantity + 1)} > + </Button>
-                                                {/*<Select
-                                                    labelId="demo-simple-select-label"
-                                                    id={"demo-simple-select" + card.id}
-                                                    value={age}
-                                                    onChange={handleChange}
-                                                >
-                                                    <MenuItem value={10}>1</MenuItem>
-                                                    <MenuItem value={20}>2</MenuItem>
-                                                    <MenuItem value={30}>3</MenuItem>
-                                                    <MenuItem value={30}>4</MenuItem>
-                                                    <MenuItem value={30}>5</MenuItem>
-                                                    <MenuItem value={30}>6</MenuItem>
-                                                    <MenuItem value={30}>7</MenuItem>
-                                                    <MenuItem value={30}>8</MenuItem>
-                                                    <MenuItem value={30}>9</MenuItem>
-                                                    <MenuItem value={30}>10</MenuItem>
-                                                </Select>*/}
                                                 <Typography variant="body2" color="textSecondary">
                                                     {product.length > 1 ? (<div>{product[card.prod_id - 1].cathegory}</div>) : (<div></div>)}
                                                 </Typography>
+                                                <Button type='button' size='small' onClick={() => handleUpdateQty(card.prod_id, card.quantity - 1)} > - </Button>
+                                                <Typography> {card.quantity} </Typography>
+                                                <Button type='button' size='small' onClick={() => handleUpdateQty(card.prod_id, card.quantity + 1)} > + </Button>
                                             </Grid>
                                             <Grid item>
-                                                <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                                                <Button id={"buttonDelete" + card.prod_id} onClick={() => handleDelete(card.prod_id)}>
                                                     Remove
-                                                </Typography>
+                                                </Button>
                                             </Grid>
                                         </Grid>
                                         <Grid item>
-                                            <Typography variant="subtitle1">$19.00</Typography>
+                                            <Typography variant="subtitle1">{product.length > 1 ? (<div>{Number(product[card.prod_id - 1].price)*Number(card.quantity)}р</div>) : (<div></div>)}</Typography>
                                         </Grid>
                                     </Grid>
                                 </Grid>
@@ -164,7 +138,7 @@ function Basket(props) {
                         ))}
                     </Grid>
                     <div><Typography gutterBottom variant="subtitle1">
-                        Общая цена:
+                        Общая цена: {props.carts.map((card, index) => card.sum)}
                     </Typography>
                     </div>
                     <Formalization_button />
@@ -174,28 +148,6 @@ function Basket(props) {
     )
 }
 
-/*
-<Grid container direction="column" alignItems="center" xs={8} sm={4} md={6}>
-<Card className={classes.card}>
-    <CardMedia component={Link} style={{ textDecoration: 'none' }} to={"/product"}
-        className={classes.cardMedia}
-        image="https://i.postimg.cc/zvJpD5vp/272d6857-eb13-4756-a6b9-7d7594f9c2df.jpg"
-        //image="https://source.unsplash.com/random"
-        title="Image title" />
-    <CardContent component={Link} style={{ textDecoration: 'none' }} to={"/product"} className={classes.cardContent}>
-        <Typography variant="h5" gutterBottom>
-            {card[0]}
-        </Typography>
-        <Typography>
-            {card[1]}
-        </Typography>
-    </CardContent>
-    <CardActions>
-        <Product_button />
-    </CardActions>
-</Card>
-</Grid>*/
-
 const mapStaterToProps = state => {
     return {
         carts: state.carts,
@@ -204,7 +156,9 @@ const mapStaterToProps = state => {
 }
 const mapDispathToProps = dispatch => {
     return {
-        cartsFetchData: url => dispatch(cartsFetchData(url))
+        cartsFetchData: url => dispatch(cartsFetchData(url)),
+        deleteCartsFetchData: url => dispatch(cartsFetchDataDelete(url)),
+        putQuantityCartsFetchData: url => dispatch(cartsFetchDataPutQuantity(url))
     };
 }
 export default connect(mapStaterToProps, mapDispathToProps)(Basket);
